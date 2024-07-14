f0971cdd322147fb0096f024c8e539942bcadaf0
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-14 19:42:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,5 +1,5 @@
-API_ID       = 19094598
-API_HASH     = "479c00e863915ba2f8b90d8030ca4786"
+API_ID       = 14641459
+API_HASH     = "478fde2c05612b0df6fc2e897b7dfe5d"
 BOT_TOKEN    = "7329951541:AAFt6_RocyG4hUHaHyRJApHbk6Tzhp_E20k"
 SESSION      = "BQA4YMoylsyYoNZucfQp9YKVTPBaIBCDGynXCOXRSc2cbLlPA2K3omNKBbJRlL7WYV5MmKbCTfq7_n5x3zkZ50cV9JgpYnfELOceaCEGrXdxZHn-8Y3KUi2WiVY1QK97TQQ5oF9knunJaoOAZwj_j7PUZxVPe6S2D2wZ3997ajgvaifAWurES6autIkg0R89strnuYaCGo_sVzpeGHamdgS1kaV_gN4jUdTfz8Mk67SeumJbFTOpuRaVeY0cLwCopEil1wpfq2xOqOOnka4vHlH391Thzap7QAR1cwQB3q9dmfELkThGjcEhDeWEr2NYMseyxy8IGYt_Ug4cNxvyGIOHAAAAATUrb2IA"
 DATABASE_URI = "mongodb+srv://anmol:anmol@cluster0.eaq9icz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
