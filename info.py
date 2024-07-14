c0f90b3cdd44c3d4ee4323d61a9d5ec6256a6b2f
ZEUSxCODES <127749230+ZEUSxCODES@users.noreply.github.com>
2024-07-14 20:06:24 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,35 +1,8 @@
-import os
-
-API_ID = os.getenv('API_ID')
-if API_ID is None:
-    raise ValueError("API_ID is not set")
-
-API_HASH = os.getenv('API_HASH')
-if API_HASH is None:
-    raise ValueError("API_HASH is not set")
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-if BOT_TOKEN is None:
-    raise ValueError("BOT_TOKEN is not set")
-
-SESSION = os.getenv('SESSION')
-if SESSION is None:
-    raise ValueError("SESSION is not set")
-
-DATABASE_URI = os.getenv('DATABASE_URI')
-if DATABASE_URI is None:
-    raise ValueError("DATABASE_URI is not set")
-
-LOG_CHANNEL = os.getenv('LOG_CHANNEL')
-if LOG_CHANNEL is None:
-    raise ValueError("LOG_CHANNEL is not set")
-
-ADMIN = os.getenv('ADMIN')
-if ADMIN is None:
-    raise ValueError("ADMIN is not set")
-else:
-    ADMIN = int(ADMIN)
-
-CHANNEL = os.getenv('CHANNEL')
-if CHANNEL is None:
-    raise ValueError("CHANNEL is not set")
+API_ID = "19094598"
+API_HASH = "479c00e863915ba2f8b90d8030ca4786"
+BOT_TOKEN = "7329951541:AAFt6_RocyG4hUHaHyRJApHbk6Tzhp_E20k"
+SESSION = "BQDfaTMAoxeB5UXzPl6N_zydZlD6g"
+DATABASE_URI = "mongodb+srv://anmol:anmol@cluster0.eaq9icz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+LOG_CHANNEL = "-1002114115777"
+ADMIN = 1663603208
+CHANNEL = "@Film_Nest"
